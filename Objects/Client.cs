07e4fe7d35d64463acb9c5c105769363416aa7c9
Ryan Mathisen <ryantmathisen@gmail.com>
2016-12-09 08:53:15 -0800

    add Client and Stylist classes with respective constructors

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -1,4 +1,19 @@
+using System;
+using System.Data.SqlClient;
+
 namespace Salon.Objects
 {
+  public class Client
+  {
+    public int Id {get; set;}
+    public string Name {get;set;}
+    public int StylistId {get; set;}
 
+    public Client(string cName, int sId, int cId = 0;)
+    {
+      this.Name = cName;
+      this.StylistId = sId;
+      this.Id = cId;
+    }
+  }
 }
--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -1,4 +1,17 @@
+using System;
+using System.Data.SqlClient;
+
 namespace Salon.Objects
 {
-  
+  public class Stylist
+  {
+    public int Id {get; set;}
+    public string Name {get;set;}
+
+    public Stylist (string sName, int sId = 0)
+    {
+      this.Name = sName;
+      this.id = sId;
+    }
+  }
 }
