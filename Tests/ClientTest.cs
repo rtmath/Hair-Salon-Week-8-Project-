1d58f81f7e8d3288f88bdf8c6a28abe17497147a
Ryan Mathisen <ryantmathisen@gmail.com>
2016-12-09 11:18:04 -0800

    add First Test (passing) for Client class GetAll method

--- Tests/ClientTest.cs
+++ Tests/ClientTest.cs
@@ -1,18 +1,28 @@
-// using Xunit;
-// using System;
-//
-// namespace Salon.Tests
-// {
-//   public class ClientTests : IDisposable
-//   {
-//     public void ClientTest()
-//     {
-//       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
-//     }
-//
-//     public void Dispose()
-//     {
-//       //DeleteAll()
-//     }
-//   }
-// }
+using Xunit;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using Salon.Startup;
+using Salon.Objects;
+
+namespace Salon.Tests
+{
+  public class ClientTests : IDisposable
+  {
+    public void ClientTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DBReturnsEmptyAtFirst()
+    {
+      Assert.Equal(0, Client.GetAll().Count);
+    }
+
+    public void Dispose()
+    {
+      //DeleteAll() here
+    }
+  }
+}
