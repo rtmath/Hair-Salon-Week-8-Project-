4848d6eed73be34cf388497082344d5c98d9f3c3
Ryan Mathisen <ryantmathisen@gmail.com>
2016-12-09 13:05:37 -0800

    add Form functionality for adding both Stylists and Clients, that returns a list of Stylists or Clients respectively to the index (this is merely for testing, will migrate these lists into their respective parts of the site next)

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,5 +1,6 @@
 using Nancy;
 using System.Collections.Generic;
+using Salon.Objects;
 
 namespace Salon.Modules
 {
@@ -9,7 +10,25 @@ namespace Salon.Modules
     {
       Get["/"] = _ => View["index.cshtml"];
       Get["/new_stylist"] = _ => View["new_sform.cshtml"];
-      Get["/new_client"] = _ => View["new_cform.cshtml"];
+      Get["/new_client"] = _ =>
+      {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["new_cform.cshtml", allStylists];
+      };
+      Post["/add_stylist"] = _ =>
+      {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        List<Stylist> stylistList = Stylist.GetAll();
+        return View["index.cshtml", stylistList];
+      };
+      Post["/add_client"] = _ =>
+      {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"]);
+        newClient.Save();
+        List<Client> clientList = Client.GetAll();
+        return View["index.cshtml", clientList];
+      };
     }
   }
 }
